5d5179e8f83ee9d5d3e13f36b083441bcb352389
jeli <elacroix@gaming.tech>
2023-04-13 11:54:23 +0200

    commity

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 import os
-os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
+x= 0
+y = 0
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+
 pygame.init()
 pygame.font.init()
-ecran = pygame.display.set_mode((910, 540), pygame.RESIZABLE)
+ecran = pygame.display.set_mode((1920, 1080), pygame.NOFRAME)
 pygame.display.set_caption("Birds Of Chaos")
 icon = pygame.image.load("ressources/img/dodo.png").convert_alpha()
 pygame.display.set_icon(icon)
@@ -19,12 +22,6 @@ jouer = True
 while jouer:
     pygame.draw.rect(ecran, (0, 0, 0), (0, 0, 1920, 1080))
     for event in pygame.event.get():
-        
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
-            ecran = pygame.display.set_mode((1920, 1080), pygame.NOFRAME)
-        elif ecran == pygame.display.set_mode((1920, 1080), pygame.NOFRAME) and event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
-            ecran = pygame.display.set_mode((910, 540), pygame.RESIZABLE)
-            
         if event.type == pygame.QUIT:
             jouer = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
