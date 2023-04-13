f94d9766f9df322560b9367890bf2e9eb6df27a1
jeli <elacroix@gaming.tech>
2023-04-13 12:08:22 +0200

    commit

--- main.py
+++ main.py
@@ -10,17 +10,16 @@ ecran = pygame.display.set_mode((1920, 1080), pygame.NOFRAME)
 pygame.display.set_caption("Birds Of Chaos")
 icon = pygame.image.load("ressources/img/dodo.png").convert_alpha()
 pygame.display.set_icon(icon)
-
-option = pygame.draw.rect(ecran, (255, 255, 255), (0, 0, 50, 50))
-
+option = pygame.image.load("ressources/img/option.png").convert_alpha()
+option_pos = (1700, 50)
 mouse = pygame.image.load("ressources/img/curseur.png").convert_alpha()
-pygame.mouse.set_visible(0)
 mouse_pos = (0, 0)
+pygame.mouse.set_visible(0)
 
 jouer = True
 
 while jouer:
-    pygame.draw.rect(ecran, (0, 0, 0), (0, 0, 1920, 1080))
+    pygame.draw.rect(ecran, (255, 255, 255), (0, 0, 1920, 1080))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jouer = False
@@ -31,6 +30,7 @@ while jouer:
         if event == pygame.MOUSEBUTTONDOWN in pygame.MOUSEMOTION(0, 0, 50, 50):
             pygame.draw.rect(ecran, (255, 255, 255), (0, 0, 50, 50))
     ecran.blit(mouse, mouse_pos)
+    ecran.blit(option, option_pos)
     pygame.display.flip()
 
 pygame.quit()
