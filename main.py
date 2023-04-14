2bfe3d2687e9e1cf93dad938ab57201b64aaba97
jeli <elacroix@gaming.tech>
2023-04-14 08:57:21 +0200

    commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("Birds Of Chaos")
 icon = pygame.image.load("ressources/img/dodo.png").convert_alpha()
 pygame.display.set_icon(icon)
 option = pygame.image.load("ressources/img/option.png").convert_alpha()
-option_pos = (700, 300)
+option_pos = (1600, 150)
 mouse = pygame.image.load("ressources/img/curseur.png").convert_alpha()
 mouse_pos = (0, 0)
 pygame.mouse.set_visible(0)
@@ -24,8 +24,8 @@ while jouer:
             jouer = False
         if event.type == pygame.MOUSEMOTION:
             mouse_pos = event.pos
-        if event == pygame.MOUSEBUTTONDOWN in pygame.MOUSEMOTION(0, 0, 50, 50):
-            pygame.draw.rect(ecran, (255, 255, 255), (0, 0, 50, 50))
+        if event == pygame.MOUSEBUTTONDOWN == option:
+            pygame.draw.rect(ecran, (0, 0, 0), (0, 0, 100, 100))
     ecran.blit(mouse, mouse_pos)
     ecran.blit(option, option_pos)
     pygame.display.flip()
