093e27d489d128db524f130f2704391dbff45d7f
Rhivan <apreynat@gaming.tech>
2023-04-15 14:08:05 +0200

    déplacement continu + collisions

---
+++ character.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Rectangle:
+    def __init__(self, screen, x, y, width, height, speed):
+        self.screen = screen
+        self.rect = pygame.Rect(x, y, width, height)
+        self.speed = speed
+
+    def move_up(self):
+        self.rect.move_ip(0, -self.speed)
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def move_down(self):
+        self.rect.move_ip(0, self.speed)
+        if self.rect.bottom > self.screen.get_height():
+            self.rect.bottom = self.screen.get_height()
+
+    def move_left(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def move_right(self):
+        self.rect.move_ip(self.speed, 0)
+        if self.rect.right > self.screen.get_width():
+            self.rect.right = self.screen.get_width()
+
+    def draw(self, color):
+        pygame.draw.rect(self.screen, color, self.rect)
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
-
+from character import *
 
 pygame.init()
+
+
+screen_width = 1920
+screen_height = 1080
+
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
 pygame.font.init()
-ecran = pygame.display.set_mode((1920, 1080), pygame.NOFRAME)
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
 pygame.display.set_caption("Birds Of Chaos")
 icon = pygame.image.load("ressources/img/dodo.png").convert_alpha()
 pygame.display.set_icon(icon)
@@ -13,21 +22,55 @@ mouse = pygame.image.load("ressources/img/curseur.png").convert_alpha()
 mouse_pos = (0, 0)
 pygame.mouse.set_visible(0)
 
-jouer = True
 
-while jouer:
-    pygame.draw.rect(ecran, (255, 255, 255), (0, 0, 1920, 1080))
+rect_width = 50
+rect_height = 100
+rect_x = (screen_width - rect_width) // 2
+rect_y = (screen_height - rect_height) // 2
+rect_speed = 1
+rect = Rectangle(screen, rect_x, rect_y, rect_width, rect_height, rect_speed)
+
+
+keys = []
+
+
+running = True
+while running:
+    # Gestion des événements pygame
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            jouer = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            jouer = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            keys.append(event.key)
+        elif event.type == pygame.KEYUP:
+            keys.remove(event.key)
         if event.type == pygame.MOUSEMOTION:
             mouse_pos = event.pos
         if event == pygame.MOUSEBUTTONDOWN == option:
-            pygame.draw.rect(ecran, (0, 0, 0), (0, 0, 100, 100))
-    ecran.blit(mouse, mouse_pos)
-    ecran.blit(option, option_pos)
+            pygame.draw.rect(screen, (0, 0, 0), (0, 0, 100, 100))
+
+    # Déplacement continu avec collisions
+    if pygame.K_UP in keys:
+        rect.move_up()
+    if pygame.K_DOWN in keys:
+        rect.move_down()
+    if pygame.K_LEFT in keys:
+        rect.move_left()
+    if pygame.K_RIGHT in keys:
+        rect.move_right()
+
+    # Effacement de l'écran
+    screen.fill(white)
+
+    # Dessin du rectangle
+    rect.draw(black)
+
+    # Rafraîchissement de l'affichage
+    screen.blit(mouse, mouse_pos)
+    screen.blit(option, option_pos)
     pygame.display.flip()
+
+# Fermeture de pygame
 pygame.quit()
+
 
