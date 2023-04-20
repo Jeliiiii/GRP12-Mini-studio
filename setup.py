1f4823f21bb3ee6a12d7b69d2e46d1fb01e4f562
deadly <vgauter@gaming.tech>
2023-04-20 19:56:17 +0200

    Simon puzzle done from scratch :3

---
+++ setup.py
@@ -0,0 +1,20 @@
+import pygame
+
+# Initialization of diverse pygame functionnalities
+
+pygame.init()
+pygame.font.init()
+
+
+# Setting up screen size
+
+userScreenInfo = pygame.display.Info()
+spyScreen_dimension = (userScreenInfo.current_w//2, userScreenInfo.current_h//2)
+spyScreen = pygame.display.set_mode(spyScreen_dimension, pygame.NOFRAME)
+pygame.display.set_caption("Birds Of Chaos")
+
+
+# Some color declaration
+
+black = (0, 0, 0)
+white = (255, 255, 255)
