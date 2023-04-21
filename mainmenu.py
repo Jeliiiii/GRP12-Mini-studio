37d265ec8687b749865ccfe511ba05e405ea51d1
Minipotal <Msylvestre52600@gmail.com>
2023-04-21 15:09:15 +0200

    mainmenu add

---
+++ mainmenu.py
@@ -0,0 +1,34 @@
+import pygame
+from window import *
+
+class MainMenu:
+    def __init__(self, window):
+        self.window = window
+        self.font = pygame.font.Font(None, 50)
+        self.play_button = pygame.Rect(200, 200, 200, 100) # Crée un rectangle pour le bouton "play"
+
+    def draw(self):
+        self.window.screen.fill((255, 255, 255)) # Efface l'écran
+        # Dessine le texte "Birds of Chaos" centré en haut de l'écran
+        text = self.font.render("Birds of Chaos", True, (0, 0, 0))
+        text_rect = text.get_rect(center=(self.window.width/2, self.window.height/4))
+        self.window.screen.blit(text, text_rect)
+
+        # Dessine le bouton "play"
+        pygame.draw.rect(self.window.screen, (0, 255, 0), self.play_button)
+        text = self.font.render("Play", True, (0, 0, 0))
+        text_rect = text.get_rect(center=self.play_button.center)
+        self.window.screen.blit(text, text_rect)
+
+        pygame.display.flip()
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Vérifie si le bouton "play" a été cliqué
+                if self.play_button.collidepoint(event.pos):
+                    return True # Retourne True pour indiquer que le jeu doit être lancé
+        return False # Retourne False sinon
