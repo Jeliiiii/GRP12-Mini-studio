c3e5ac7532b1ac870b9a54789ef5caacef5ae2a1
BOISMENU Bapiste <bboismenu@gaming.tech>
2023-04-21 19:17:24 +0200

    First 2 - DONE ! => fresh window perfect display + hitbox

--- operator screen.py
+++ operator screen.py
@@ -50,34 +50,55 @@ class PseudoWindow:
         self.color = color
         self.borderSize = 5
         self.menuSize = 20
+        # self.priority = ???
 
-        # Creates the "window" components for future displaying. Calculus are present to size and position Surf correctly within the Rects
-        self.rectBorder = pygame.Rect(self.coord, self.dim)
+        
+        """
+        Creates the "window" components. surfComponent are here to blit images within them, while rectComponent are used as hitboxes
+        """
+        # Hitbox for the entire window
+        self.rectWhole = pygame.Rect(self.coord, self.dim)
+
+        # Hitbox of the menu, or band on the top
         self.rectMenu = pygame.Rect(self.coord, (self.dim[0], self.menuSize))
-        self.surfContent = pygame.Surface((self.dim[0] - 2*self.borderSize, self.dim[1] - self.borderSize - self.menuSize))
-        self.rectContent = pygame.Surface.get_rect()
-        self.surfContent.fill(white)
-        self.Cross = pygame.Surface((16, 16))
-        # self.Cross.fill((255, 0, 0))
 
+        # Hitbox and Surface for the inside of the window
+        self.surfContent = pygame.Surface((self.dim[0] - 2*self.borderSize, self.dim[1] - self.borderSize - self.menuSize)) # Inside of the window
+        self.surfContent.fill(white)
+        self.rectContent = self.surfContent.get_rect(topleft = (self.coord[0] + self.borderSize, self.coord[1] + self.menuSize))
+
+        # Hitbox and surface for the cross to close the window
+        self.surfCross = pygame.Surface((16, 16))
+        self.surfCross.fill((255, 0, 0))
+        self.rectCross = self.surfCross.get_rect(topright = (self.coord[0] + self.dim[0] - self.borderSize, self.coord[1] + (self.menuSize - self.surfCross.get_height()) // 2))
+
+        # Adding the new instance to the list
         PseudoWindow.loadedPseudoWindows.append(self)
 
     def __del__(self):
-        print("Called destructor on \"window\"")
+        print("/!\\ Called destructor /!\\")
 
-    def __str__(self):
-        return f"coordinates: {self.coord}, dimensions: {self.dim}, close method: {self.closeCond}, color: {self.color}, border size: {self.borderSize}, menu size: {self.menuSize}"
+    # def __str__(self):
+    #     return f"coordinates: {self.coord}, dimensions: {self.dim}, close method: {self.closeCond}, color: {self.color}, border size: {self.borderSize}, menu size: {self.menuSize}"
 
     def show(self):
-        pygame.draw.rect(spyScreen, self.color, self.rectBorder, self.borderSize)
+        # Draws the effective border to the window
+        pygame.draw.rect(spyScreen, self.color, self.rectWhole)
+
+        # Draw the top band to look like a menu
         pygame.draw.rect(spyScreen, self.color, self.rectMenu)
-        pygame.draw.rect(spyScreen, (255, 0, 0), self.Cross.get_rect())
-        spyScreen.blit(self.surfContent, (self.coord[0] + self.borderSize, self.coord[1] + self.menuSize))
-        # spyScreen.blit(self.Cross, (self.coord[0] + self.dim[0] - self.borderSize - 18, self.coord[1] + 2))
+
+        # Blit the Content of the window
+        # spyScreen.blit(self.surfContent, (self.coord[0] + self.borderSize, self.coord[1] + self.menuSize))
+        spyScreen.blit(self.surfContent, (self.rectContent.x, self.rectContent.y))
+
+        # Blit the Cross of the window
+        spyScreen.blit(self.surfCross, (self.rectCross.x, self.rectCross.y))
+
 
 # Carthage((100, 100), (300, 300), color = (45, 177, 88, 1), menuSize = 16, borderSize = 8)
 # Simon((300, 100), (300, 300), color = (45, 177, 88, 1), menuSize = 16, borderSize = 8)
-PseudoWindow((600, 100), (300, 300), color = (45, 177, 88, 1), menuSize = 16, borderSize = 8)
+PseudoWindow((100, 100), (300, 300), color = (45, 177, 88, 1), menuSize = 16, borderSize = 8)
     
 
 # Boucle de jeu
@@ -111,7 +132,7 @@ while running:
             if pygame.mouse.get_pressed(3)[0]:
                 print("Liste des fenêtres chargés :\n", PseudoWindow.loadedPseudoWindows)
                 for cur in range(len(PseudoWindow.loadedPseudoWindows)):
-                    if PseudoWindow.loadedPseudoWindows[cur].Cross.get_rect().collidepoint(mouse_pos):
+                    if PseudoWindow.loadedPseudoWindows[cur].surfCross.get_rect().collidepoint(mouse_pos):
                         del PseudoWindow.loadedPseudoWindows[cur]
                         print("element removed from list")
                 #     elif instance.surfContent.get_rect().collidepoint(mouse_pos):
