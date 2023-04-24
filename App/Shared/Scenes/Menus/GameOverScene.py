20e058baa31097dbd1d602223491e49179757a21
mlipko <mlipko@gaming.tech>
2023-04-24 08:19:13 +0200

    ecran de game over

---
+++ App/Shared/Scenes/Menus/GameOverScene.py
@@ -0,0 +1,27 @@
+from Shared.Actors.UI.ButtonActor import ButtonActor
+from .MenuScene import MenuScene
+import pygame
+
+
+class GameOverScene(Scene):
+    def __init__(self):
+        super().__init__("GameOver")
+   
+        self.menu.buttonsList=[ButtonActor("Back to menu", self.switchMainMenuScene)]
+
+
+
+
+    def updateScene(self, inputs, dt):
+        self.menu.handleMouse(inputs["MOUSE_POS"][0], inputs["MOUSE_POS"][1], inputs["MOUSE_BUTTONS"])
+        self.HUD[1].onTick(inputs, dt)
+
+
+    def drawScene(self, window):
+        window.fill( "#111126" )
+        self.menu.draw(window)
+
+
+    def switchMainMenuScene(self):
+        from .MainMenuScene import MainMenuScene
+        self.nextScene = MainMenuScene()
