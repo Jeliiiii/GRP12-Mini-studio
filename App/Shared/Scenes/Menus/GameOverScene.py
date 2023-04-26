9537997bcfb2cba813fd6192cffcb3d8d52b8a47
valentinr <vgautier@gaming.tech>
2023-04-26 16:46:19 +0200

    ned to sync

--- App/Shared/Scenes/Menus/GameOverScene.py
+++ App/Shared/Scenes/Menus/GameOverScene.py
@@ -29,6 +29,7 @@ class GameOverScene(MenuScene):
 
 
     def drawScene(self, window):
+        window.fill( "#111126" )
         window.blit(self.backGround, self.screenSize)
         self.menu.draw(window)
 
